f77e6a493d8d50d238338a6f3adb9fd110f02655
wpaq <wallysonpabloo@gmail.com>
2021-11-20 20:38:52 -0300

    ajusts server

--- src/app.js
+++ src/app.js
@@ -3,6 +3,8 @@ const express = require('express');
 class AppController {
     constructor() {
         this.express = express();
+        this.middlewares();
+        this.routes();
     }
 
     middlewares() {
