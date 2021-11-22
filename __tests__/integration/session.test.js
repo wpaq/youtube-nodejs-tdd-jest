4fdba9c827a325bb8c6a81ccd1c352efa148e12f
wpaq <wallysonpabloo@gmail.com>
2021-11-22 19:28:43 -0300

    .env.test configuration; SessionController add;

---
+++ __tests__/integration/session.test.js
@@ -0,0 +1,28 @@
+const request = require('supertest');
+
+const app = require('../../src/app');
+const { User } = require('../../src/app/models');
+const truncate = require('../utils/truncate');
+
+describe('Authentication', () => {
+    beforeEach(async () => {
+        await truncate();
+    });
+
+    it('should authenticate with valid credentials', async () => {
+        const user = await User.create({
+            name: 'Wallyson',
+            email: 'wallysonn@email.com',
+            password_hash: '123123',
+        });
+
+        const response = await request(app)
+            .post('/sessions')
+            .send({
+                email: user.email,
+                password: '123456'
+            });
+        
+        expect(response.status).toBe(200);
+    });
+});
--- src/routes.js
+++ src/routes.js
@@ -1,6 +1,7 @@
 const routes = require('express').Router();
-const { User } = require('./app/models')
 
-User.create({ name: 'Wallyson', email: 'wallyson@email.com', password_hash: '5585454545' });
+const SessionController = require('./app/controllers/SessionController')
+
+routes.post('/sessions', SessionController.store);
 
 module.exports = routes;
